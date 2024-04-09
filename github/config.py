3ea585163eeec68a5e7bdf7f0afdb34f05131340
ZAXph <152623389+ZAXph@users.noreply.github.com>
2024-04-09 14:38:56 +0300

    Update config.py

--- github/config.py
+++ github/config.py
@@ -3,7 +3,7 @@ from telebot import *
 system_content = ("Ты — дружелюбный помощник во всем! А так же можешь поддерживать диалог! А так же ты поддерживаешь "
                   "только русский язык.")
 assistant_content = "Давай разберем по шагам: "
-token = "6815594086:AAFmwexlJBjfNt8xinJKVhUz2613ND2opX0"
+token = ""
 
 
 def check_next(message):
